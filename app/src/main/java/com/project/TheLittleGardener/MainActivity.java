51cecdc6457a6307e845866d4419133499f3d7eb
Michał Sakowicz <michalsakowicz99@gmail.com>
2020-10-18 01:14:07 +0200

    main Menu GUI

--- app/src/main/java/com/project/TheLittleGardener/MainActivity.java
+++ app/src/main/java/com/project/TheLittleGardener/MainActivity.java
@@ -2,7 +2,11 @@ package com.project.TheLittleGardener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Application;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playAction(View view)
+    {
+        //go to next scene
+    }
+
+    public void exitGame (View view)
+    {
+        finish();
     }
 }
