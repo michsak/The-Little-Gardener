aedd6814abeda55116dfb29e40882d56ae0b0458
Michał Sakowicz <michalsakowicz99@gmail.com>
2020-10-31 03:21:27 +0100

    working joystick

--- app/src/main/java/com/project/TheLittleGardener/PlayGame.java
+++ app/src/main/java/com/project/TheLittleGardener/PlayGame.java
@@ -2,25 +2,31 @@ package com.project.TheLittleGardener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.content.Context;
 import android.graphics.Point;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class PlayGame extends AppCompatActivity
-{
+import com.zerokol.views.joystickView.JoystickView;
+
+
+public class PlayGame extends AppCompatActivity {
     protected MediaPlayer mediaPlayer;
     protected MusicManagement gameMusicManagement;
-    private GameView gameView;
+    private ImageView player;
+
+    private TextView angleTextView;
+    private TextView powerTextView;
+    private TextView directionTextView;
+    private JoystickView joystick;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
 
@@ -33,35 +39,68 @@ public class PlayGame extends AppCompatActivity
         Point point = new Point();
         getWindowManager().getDefaultDisplay().getSize(point);
 
-        //starts game loop, player movements, updating, etc.
-        gameView = new GameView(this, point.x, point.y);
 
-        //show view on the screen
-        setContentView(gameView);
-
-
+        player = findViewById(R.id.playerView);
+        Button moveUp = findViewById(R.id.moveUpButton);
         //hide navigation bar and support bar
         /*View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
         getSupportActionBar().hide();
     */
-    }
 
-    //stop game
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        gameView.pause();
-    }
+        angleTextView = (TextView) findViewById(R.id.angleTextView);
+        powerTextView = (TextView) findViewById(R.id.powerTextView);
+        directionTextView = (TextView) findViewById(R.id.directionTextView);
+        //Referencing also other views
+        joystick = (JoystickView) findViewById(R.id.joystickView);
 
-    //resume to game
-    @Override
-    protected void onResume()
+        //Event listener that always returns the variation of the angle in degrees, motion power in percentage and direction of movement
+        joystick.setOnJoystickMoveListener(new JoystickView.OnJoystickMoveListener() {
+
+            @Override
+            public void onValueChanged(int angle, int power, int direction) {
+                // TODO Auto-generated method stub
+                angleTextView.setText(" " + String.valueOf(angle) + "°");
+                powerTextView.setText(" " + String.valueOf(power) + "%");
+                switch (direction) {
+                    case JoystickView.FRONT:
+                        directionTextView.setText(R.string.front_lab);
+                        break;
+                    case JoystickView.FRONT_RIGHT:
+                        directionTextView.setText(R.string.front_right_lab);
+                        break;
+                    case JoystickView.RIGHT:
+                        directionTextView.setText(R.string.right_lab);
+                        break;
+                    case JoystickView.RIGHT_BOTTOM:
+                        directionTextView.setText(R.string.right_bottom_lab);
+                        break;
+                    case JoystickView.BOTTOM:
+                        directionTextView.setText(R.string.bottom_lab);
+                        break;
+                    case JoystickView.BOTTOM_LEFT:
+                        directionTextView.setText(R.string.bottom_left_lab);
+                        break;
+                    case JoystickView.LEFT:
+                        directionTextView.setText(R.string.left_lab);
+                        break;
+                    case JoystickView.LEFT_FRONT:
+                        directionTextView.setText(R.string.left_front_lab);
+                        break;
+                    default:
+                        directionTextView.setText(R.string.center_lab);
+                }
+            }
+        }, JoystickView.DEFAULT_LOOP_INTERVAL);}
+
+
+    public void moveUp(View view)
     {
-        super.onResume();
-        gameView.resume();
+        TranslateAnimation animation = new TranslateAnimation(0.0f, 50.0f, 0.0f, 0.0f);
+        animation.setDuration(700);
+        animation.setFillAfter(true);
+        player.startAnimation(animation);
     }
 
 }
